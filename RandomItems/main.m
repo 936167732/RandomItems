1483df754d792c76292c536b5654121f5f318575
谢海邦 <X@xiehaibangdemacbook-pro.local>
2016-07-02 15:57:38 +0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by 谢海邦 on 16/7/2.
+//  Copyright © 2016年 谢海邦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
